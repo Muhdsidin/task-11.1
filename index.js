09727a9f44e375ecef1fdb9aee063657d304c9f4
sidin <officialzedro@gmail.com>
2023-05-18 23:11:34 +0530

    task -11.1

---
+++ index.js
@@ -0,0 +1,5 @@
+const util =require("./util")
+util.Addition(10,20)
+
+util.Multiply(10,20)
+util.Substraction(10,20)
---
+++ util.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    Addition:(a , b)=>{
+        console.log(a + b , "==Addition")
+    },
+    Substraction:(a,b)=>{
+        console.log(a-b , "==sub")
+    },
+    Multiply:(a,b)=>{
+        console.log(a*b , "==multy")
+    }
+}
